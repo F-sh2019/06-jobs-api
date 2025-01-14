7e1ad545feffdedf038ae0b9576e9bd39c7bf0b5
Filicity SH <f.elena.sh@gmail.com>
2025-01-14 15:13:53 -0800

    password hash and Length updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,20 @@
 const User= require('../models/User')
 const {StatusCodes} = require('http-status-codes')
 const {BadRequestError} = require ('../errors')
-
+const bcrypt =require('bcryptjs')
 
 const register= async (req , res) => {
-    const user = await User.create({ ...req.body })
     const {name , email , password} = req.body
+
+    
+    const salt = await bcrypt.genSalt(10) ;
+    const hasshedPassword = await bcrypt.hash(password, salt)
+
+    const tempUser = {name , email, password:hasshedPassword } 
+
+    const user = await User.create({ ...tempUser })
+    
+
 
 if (!name || !email ||!password){
     throw new BadRequestError('Please provide name, email and password')
