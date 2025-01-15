6f07b64cf4c6fedbc6b5e7f9457fff6b7903077c
Filicity SH <f.elena.sh@gmail.com>
2025-01-14 16:53:34 -0800

    password compared and .env file updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,33 +1,33 @@
-const User= require('../models/User')
-const {StatusCodes} = require('http-status-codes')
-const {BadRequestError} = require ('../errors')
-const bcrypt =require('bcryptjs')
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 
-const register= async (req , res) => {
-    const {name , email , password} = req.body
-
-    
-    const salt = await bcrypt.genSalt(10) ;
-    const hasshedPassword = await bcrypt.hash(password, salt)
-
-    const tempUser = {name , email, password:hasshedPassword } 
-
-    const user = await User.create({ ...tempUser })
-    
-
-
-if (!name || !email ||!password){
-    throw new BadRequestError('Please provide name, email and password')
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
-  console.log("email checked")
-  res.status(StatusCodes.CREATED).json({user}) ;
 
+const login = async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid Credentials')
+  }
  
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
-
-const login = async (req , res ) =>{
-    res.send(' login user')
+module.exports = {
+  register,
+  login,
 }
-
-module.exports = {register , login ,}
